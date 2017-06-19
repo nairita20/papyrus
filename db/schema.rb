0b164b2cf8f63f73a291c121916f11874fcbc736
nairita <nairita@threadsol.com>
2017-06-19 15:59:11 +0530

    Add latest feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612100831) do
+ActiveRecord::Schema.define(version: 20170619053213) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -20,4 +20,14 @@ ActiveRecord::Schema.define(version: 20170612100831) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
